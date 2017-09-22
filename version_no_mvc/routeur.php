8c785eb4f3942bb8c6416ab3cc42dc698d721850
miloon <medellyna@gmail.com>
2017-09-22 16:48:28 +0200

    ajout de commentaires explicatifs pour le router

--- version_no_mvc/routeur.php
+++ version_no_mvc/routeur.php
@@ -1,7 +1,13 @@
 <?php
-// si GET est vide, on va à l'accueil
+// si GET est vide, on va à l'accueil. Regarde également comment est codé le lien de l'accueil dans le fichier menu.php
 if(empty($_GET)){ // pas de variables get => ACCUEIL
   require_once 'accueil.php';
+
+/*
+* Pour comprendre l'accès aux pages suivantes, 
+* tu peux regardes également la page menu.php
+* Regarde comment sont notés les liens sur les deux fichiers
+*/
 
 }elseif (isset($_GET['annuaire'])){
   $contact = $_GET['annuaire'];
@@ -13,10 +19,17 @@ if(empty($_GET)){ // pas de variables get => ACCUEIL
   $factures = $_GET['factures'];
   require_once "factures.php";
 
+/*
+* Pour comprendre l'accès à la page idcontact avec l'id placé en variable
+* regarde la page annuaire.php et plus spécifiquement la ligne :
+* <a href="?idcontact=<?=$value['id_personne']?>">
+* Note la structure => on a idcontact qui passe en get
+* et quand on clique sur l'id, on stocke l'id (donc le nombre) dans la variable
+*/
 }elseif(isset($_GET['idcontact']) &&  is_numeric($_GET['idcontact'])){
   // conversion en variable locale en int
   $idcontact = (int)$_GET['idcontact'];
-  // on importe la page détail qu'on a préparée
+  // on importe la page détailcontact.php qu'on a préparée
   require_once 'detailcontact.php';
 
 }elseif(isset($_GET['idsociete']) &&  is_numeric($_GET['idsociete'])){
@@ -26,7 +39,7 @@ if(empty($_GET)){ // pas de variables get => ACCUEIL
   $idfacture = (int)$_GET['idfacture'];
   require_once 'detailfacture.php';
 
-  // rien n'est juste, on retourne à l'accueil
+  // rien n'est juste, on retourne à l'accueil. On prend en compte les possibilités de bidouillages d'URL.
 }else{
   require_once 'accueil.php';
 }
